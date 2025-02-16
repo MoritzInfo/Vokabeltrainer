030590b9f3e03537eded2288e42022bdab4734ba
erlem4 <erik.lemke06@gmail.com>
2025-02-16 17:59:06 +0100

    Add files via upload
    so ein behinderter müll. die ganze zeit kommt die nachricht https//:github ... :git-recieve-pack not permittet on https//:github ...  wenn ich das auf herr rauwolf seine seite hochladen will, obwohl alle daten korrekt sind.hzkgckktzj

---
+++ gui-copy/gui/Eintragen.java
@@ -0,0 +1,39 @@
+package gui;
+
+// package gui;
+// import java.util.Scanner;
+// import java.io.File;
+// import java.io.IOException;
+// /**
+ // * manueller User Input einer Lernkarte, falls wir das noch brauchen
+ // */
+public class Eintragen
+{
+
+    // public Eintragen()
+    // {
+
+    // }
+    
+    // public void main()
+    // {
+        // try{
+            // File file = new File("VOKABELLISTE.TXT");
+            // Lernkarte karte;
+            // Scanner scanner = new Scanner(file);  
+            // System.out.println("Vokabel eintragen");
+        
+            // String vokabel = scanner.nextLine();
+            // System.out.println("Übersetzung eintragen");
+            // String lösung = scanner.nextLine();
+            // System.out.println("Kategorie eintragen");
+            // String kategorie = scanner.nextLine();
+            // System.out.println("Neue Vokabel " + vokabel + " in " +
+            // kategorie + " eingetragen.");  
+
+            // karte = new Lernkarte(vokabel, lösung, kategorie);
+        // } catch(IOException e) {
+            // System.out.printf("Error: %s\n" , e);
+        // }
+    // }
+}
